7daba222d7b20eae9323cb8fdf12ae12b28559f8
json <1557392527@qq.com>
2025-02-12 00:46:06 +0800

    fix: 修复报错

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './api/user/user.controller';
 import { UserModule } from './api/user/user.module';
 import { TagModule } from './api/tag/tag.module';
 import mysqlConfig from './config/mysql.config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(mysqlConfig), UserModule, TagModule],
-  controllers: [UserController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
