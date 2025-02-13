147da38f656723760a7ecf2b713a5b523fa59af9
json <1557392527@qq.com>
2025-02-14 01:39:29 +0800

    feat: 数据库配置

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -7,6 +7,15 @@ const mysqlConfig: TypeOrmModuleOptions = {
   username: 'root',
   password: '',
   database: '',
+  autoLoadEntities: true, // 自动加载，注册实体
+  dateStrings: true, // 格式化的日期字符串
+  // 手动指定实体
+  // entities: [
+  //   User,
+  //   Post,
+  //   Comment,
+  //   // ... 其他实体
+  // ],
 };
 
 export default mysqlConfig;
