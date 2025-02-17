64de3771da96a63c444272c467840909a751bf5e
xiaoyu.yjq <xiaoyu.yjq@come-future.com>
2025-02-17 20:57:33 +0800

    feat: adjust the abnormal filter

---
+++ src/core/filter/all-exception.filter.ts
@@ -0,0 +1,39 @@
+// 异常过滤器
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const exceptionResponse: any = exception.getResponse();
+
+    const message = Array.isArray(exceptionResponse.message)
+      ? exceptionResponse.message.join(', ')
+      : exceptionResponse.message;
+
+    const requestDetails = {
+      query: request.query,
+      body: request.body,
+      params: request.params,
+      method: request.method,
+      url: request.url,
+      timestamp: new Date().toString(),
+      ip: request.ip,
+    };
+
+    response.status(status).json({
+      code: status,
+      data: requestDetails,
+      message,
+    });
+  }
+}
