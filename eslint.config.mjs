11f773de00b9b899d86bd11faa0f1b09c94e97eb
xiaoyu.yjq <xiaoyu.yjq@come-future.com>
2025-02-18 17:13:16 +0800

    chore: adjust tsconfig.json and eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,25 @@
-// @ts-check
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import prettier from 'eslint-plugin-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['node_modules/**', 'eslint.config.mjs'], // 忽略 node_modules 目录和 eslint.config.js 文件
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    files: ['**/*.{js,ts}'], // 处理所有 .js 和 .ts 文件
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 2020,
       sourceType: 'module',
       parserOptions: {
         projectService: true,
@@ -27,6 +29,8 @@ export default tseslint.config(
   },
   {
     rules: {
+      ...typescriptEslintPlugin.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
