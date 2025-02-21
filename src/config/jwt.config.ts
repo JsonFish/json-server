700c05f0646e50adac6fe3b1c9a122fba74d0f2f
json <1557392527@qq.com>
2025-02-22 01:24:29 +0800

    feat: add config files

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,4 @@
+export default {
+  accessSecretKey: 'accessSecretKey', // 加密accessToken
+  refreshSecretKey: 'refreshSecretKey', // 加密refreshToken
+};
--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -2,20 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const mysqlConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: '',
+  host: 'host',
   port: 3306,
-  username: 'root',
-  password: '',
-  database: '',
-  autoLoadEntities: true, // 自动加载，注册实体
-  dateStrings: true, // 格式化的日期字符串
-  // 手动指定实体
-  // entities: [
-  //   User,
-  //   Post,
-  //   Comment,
-  //   // ... 其他实体
-  // ],
+  username: 'username',
+  password: 'password',
+  database: 'database',
+  autoLoadEntities: true,
+  dateStrings: true,
 };
 
 export default mysqlConfig;
